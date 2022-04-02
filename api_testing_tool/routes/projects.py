a46037421d4675bc0cc0c0263d78e7b5978cca74
poojan-2601 <poojannaik88@gmail.com>
2022-04-03 00:55:39 +0530

    added delete projects route

--- api_testing_tool/routes/projects.py
+++ api_testing_tool/routes/projects.py
@@ -29,3 +29,19 @@ def createProjects():
         return jsonify({"success": "project created successfully"})
     else:
         return jsonify({"errors": "You already have a project of the same name"})
+
+@projects_blueprint.route('/api/delete-projects',methods=["POST"])
+@jwt_required()
+def deleteProjects():
+    data = request.json
+    project_name = data.get("name")
+    slug = create_slug(project_name)
+    if db.projects.find_one({"slug" : slug}) == None:
+        return jsonify({"errors" : "No such project exists in your project directory"})
+    else:
+        db.projects.delete_one({"slug" :slug})
+        return jsonify({"success" : "project succesfully removed"})
+
+
+
+
