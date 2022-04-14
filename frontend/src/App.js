e436edead11523a73a7d15b1703295b749cb6e77
Tanmay Thole <tanmaythole@gmail.com>
2022-04-14 13:56:02 +0530

    some changes fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,9 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<PrivateRoutes />}>
-<<<<<<< HEAD
           <Route path="/dashboard" element={<HomeContainer />} />
-          <Route path="/project/:projSlug" element={<></>} />
-=======
-          <Route path="/" element={<HomeContainer />} />
           <Route path="/project/:projSlug" element={<><ProjectsContainer/></>} />
           <Route path="/project/:projSlug/:tab" element= {<ProjectsContainer/>} />
->>>>>>> 47d90a1342553441e9af5998f5bf92e2cbf83f4b
         </Route>
         <Route path="/login" element={<LoginContainer />} />
         <Route path="/register" element={<SignupContainer />} />
