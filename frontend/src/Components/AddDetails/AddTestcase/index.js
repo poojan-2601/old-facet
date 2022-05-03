0bcf65cd55132138ce3934ae0dcd88363df42aa0
Tanmay Thole <tanmaythole@gmail.com>
2022-05-03 12:27:04 +0530

    Added React-select to add testcases in testsuite

--- frontend/src/Components/AddDetails/AddTestcase/index.js
+++ frontend/src/Components/AddDetails/AddTestcase/index.js
@@ -1,25 +1,128 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form,Button } from "react-bootstrap";
+import axios from "../../../axios";
+import Select from 'react-select';
 
 const AddTestcase = (props) => {
     const { projSlug, tab, handleClose } = props;
     
+    const [formData, setFormData] = useState(
+        {
+            "name": "",
+            "method": "",
+            "project": projSlug,
+            "endpoint_id": "",
+            "payload_id": "",
+            "header_id": ""
+        }   
+    )
+    const [options, setOptions] = useState(
+        {
+            "headers": [],
+            "endpoints": [],
+            "payloads": []
+        }
+    );
     const onSubmit = (e) => {
-        handleClose();
         e.preventDefault();
-        console.log('submit working well');
-        console.log(projSlug)
+        axios.post('/api/testcases/new', formData)
+            .then(res => {
+                alert(res.data.success);
+            })
+            .catch(err => {
+                console.log(err.response.data);
+            })
+        handleClose();
+    }
+
+    const fetchData = async () => {
+        await axios.get('/api/headers', { params: { "project": projSlug }})
+            .then(res => {
+                const data = res.data.headers;
+                let headers = [];
+                data.forEach(ele => {
+                    headers.push({value: ele['_id'], label: ele['name']})
+                })
+                setOptions({...options, "headers":headers});
+            })
+
+            await axios.get('/api/payloads', { params: { "project": projSlug }})
+                .then(res => {
+                    const data = res.data.payloads;
+                    let payloads = [];
+                    data.forEach(ele => {
+                        payloads.push({value: ele['_id'], label: ele['name']})
+                    })
+                    setOptions({...options, "payloads":payloads});
+                })
+            
+            await axios.get('/api/endpoints', { params: { "project": projSlug }})
+                    .then(res => {
+                        const data = res.data.endpoints;
+                        let endpoints = [];
+                        data.forEach(ele => {
+                            endpoints.push({value: ele['_id'], label: ele['name']})
+                        })
+                        setOptions({...options, "endpoints":endpoints});
+                    })
+    } 
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+    
+    
+    
+
+    const onchange = (e) => {
+        setFormData({...formData, [e.target.name]:e.target.value});
     }
     return (
         <>
             <Form onSubmit={onSubmit}>
                 <Form.Group className='mb-3'>
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" name="name" id="name" />
+                    <Form.Control type="text" name="name" id="name" value={formData.name} onChange={onchange} />
                 </Form.Group>
                 <Form.Group className='mb-3'>
-                    <Form.Label>Testcase</Form.Label>
-                    <Form.Control type="text" name="name" id="name" />
+                    <Form.Label>Method</Form.Label>
+                    <Form.Select name="method" id="method" value={formData.method} onChange={onchange}>
+                        <option value="" selected disabled>Select</option>
+                        <option value="GET">GET</option>
+                        <option value="POST">POST</option>
+                        <option value="PUT">PUT</option>
+                        <option value="DELETE">DELETE</option>
+                        <option value="PATCH">PATCH</option>
+                        <option value="HEAD">HEAD</option>
+                        <option value="OPTIONS">OPTIONS</option>
+                    </Form.Select>
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label>Endpoint</Form.Label>
+                    <Select 
+                        options={options.endpoints}
+                        onChange={(e) => setFormData({...formData, "endpoint_id": e.value})}
+                        name="endpoint_id"    
+                    />
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label>Headers</Form.Label>
+                    <Select 
+                        options={options.headers}
+                        onChange={(e) => setFormData({...formData, "header_id": e.value})}
+                        name="header_id"    
+                    />
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label>Payloads</Form.Label>
+                    <Select 
+                        options={options.payloads}
+                        onChange={(e) => setFormData({...formData, "payload_id": e.value})}
+                        name="payload_id"    
+                    />
                 </Form.Group>
                 <div className="d-flex justify-content-end">
                     <Button variant="secondary" onClick={handleClose} className="me-2">Close</Button>
--- frontend/src/Components/AddDetails/AddTestsuite/index.js
+++ frontend/src/Components/AddDetails/AddTestsuite/index.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form,Button } from "react-bootstrap";
 import axios from "../../../axios";
+import Select from 'react-select';
 
 const AddTestsuite = (props) => {
     const { projSlug, tab, handleClose } = props;
+
+    const [formData, setFormData] = useState(
+        {
+            "project": projSlug,
+            "name": "",
+            "description": "",
+            "testcases": []
+        }
+    )
+    const [testcaseOptions, setTestcaseOptions] = useState([]);
+
     const onSubmit = (e) => {
-        handleClose();
         e.preventDefault();
-        console.log('submit working well');
-        console.log(projSlug)
+        axios.post('/api/testsuite/new', formData)
+            .then(res => {
+                alert(res.data.success);
+            })
+            .catch(err => {
+                console.log(err.response.data)
+            })
+        handleClose();
+    }
+
+    useEffect(() => {
+        axios.get('/api/testcases', { params: { "project": projSlug }})
+            .then(res => {
+                const data = res.data.testcases;
+                let testcases = [];
+                data.forEach(ele => {
+                    testcases.push({value: ele['_id'], label: ele['name']})
+                })
+                setTestcaseOptions(testcases);
+            })
+    }, [])
+    
+    const onchange = (e) => {
+        setFormData({...formData, [e.target.name]:e.target.value});
     }
     return (
         <>
             <Form onSubmit={onSubmit}>
                 <Form.Group className='mb-3'>
-                    <Form.Label>Testsuite Name</Form.Label>
-                    <Form.Control type="text" name="name" id="name" />
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name="name" id="name" value={formData.name} onChange={onchange} />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" name="name" id="name" />
+                    <Form.Control as="textarea" rows={3} name="description" id="description" value={formData.description} onChange={onchange} />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>Testcases</Form.Label>
-                    <Form.Control type="text" name="name" id="name" />
+                    <Select 
+                        isMulti={true}
+                        options={testcaseOptions}
+                        onChange={(e) => setFormData({...formData, "testcases": e.map(data => data.value)})}
+                        name="testcases"    
+                    />
                 </Form.Group>
                 <div className="d-flex justify-content-end">
                     <Button variant="secondary" onClick={handleClose} className="me-2">Close</Button>
